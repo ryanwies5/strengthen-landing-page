a77ee799c3c436d7c4e8d8560e4c9515aaf960d8
Wiesner <wiesner@Wiesners-Mac-mini.local>
2026-03-03 18:39:18 +0800

    Add under construction page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import { Navbar } from './components/Website/Navbar'
-import { Hero } from './components/Website/Hero'
-import { Podcast } from './components/Website/Podcast'
-import { MotionGraphics } from './components/Website/MotionGraphics'
-import { Blog } from './components/Website/Blog'
-import { Coaching } from './components/Website/Coaching'
-import { Footer } from './components/Website/Footer'
+// import { Navbar } from './components/Website/Navbar'
+// import { Hero } from './components/Website/Hero'
+// import { Podcast } from './components/Website/Podcast'
+// import { MotionGraphics } from './components/Website/MotionGraphics'
+// import { Blog } from './components/Website/Blog'
+// import { Coaching } from './components/Website/Coaching'
+// import { Footer } from './components/Website/Footer'
+import { UnderConstruction } from './components/UnderConstruction'
 import './index.css'
 
 function App() {
-  return (
-    <div className="min-h-screen bg-background text-foreground antialiased">
-      <Navbar />
-      <main>
-        <Hero />
-        <Podcast />
-        <MotionGraphics />
-        <Blog />
-        <Coaching />
-      </main>
-      <Footer />
-    </div>
-  )
+  return <UnderConstruction />
+  // Full site (uncomment to restore):
+  // return (
+  //   <div className="min-h-screen bg-background text-foreground antialiased">
+  //     <Navbar />
+  //     <main>
+  //       <Hero />
+  //       <Podcast />
+  //       <MotionGraphics />
+  //       <Blog />
+  //       <Coaching />
+  //     </main>
+  //     <Footer />
+  //   </div>
+  // )
 }
 
 export default App
+
